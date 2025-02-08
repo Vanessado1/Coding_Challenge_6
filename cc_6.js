ff95e18f12ac7bb2893800517fc14e10f9146688
Vanessa Do <xex.vanessa@gmail.com>
2025-02-08 15:16:07 -0500

    Task 1- Business Profit Calculation

---
+++ cc_6.js
@@ -0,0 +1,7 @@
+// Task 1: Function Declaration 
+// created a function to calculate the profit 
+function calculateProfit(costPrice,sellingPrice,unitSold){
+    return profit = (sellingPrice - costPrice)*unitSold
+}
+console.log(`Profit: $${calculateProfit(20, 30, 100)}`); // Expected output: "Total Profit: $1000"
+console.log(`Profit: $${calculateProfit(50, 70, 200)}`); // Expected output: "Total Profit: $4000"
