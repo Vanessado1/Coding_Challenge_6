e91f740eca73440a53e98c9dfbedffd4b9928701
Vanessa Do <xex.vanessa@gmail.com>
2025-02-08 23:42:25 -0500

    Task 3- Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -13,3 +13,20 @@ let calculateSalesTax = function (amount, taxRate) {
 }
 console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
 console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
+
+// Task 3: Arrow Function 
+// created a calculate bonus arrow function 
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentages = {
+        'Excellent': 0.20,
+        'Good': 0.10,
+        'Average': 0.05
+        
+    };
+    let bonusPercentage = bonusPercentages[performanceRating] || 0;
+    const bonus = salary * bonusPercentage;
+    return bonus;
+}
+console.log(`Bonus: $${calculateBonus(5000, 'Excellent')}`); // Expected output: "Bonus: $1000"
+console.log(`Bonus: $${calculateBonus(7000, 'Good')}`);  // Expected output: "Bonus: $700"
+
